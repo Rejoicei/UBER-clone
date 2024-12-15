097aae771a68c7ebe15ad1cf940c1ec8107db271
Rejoice <rejoice.itur@xaviers.edu.in>
2024-12-15 12:29:05 -0700

    user login

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -13,5 +13,12 @@ router.post('/register', [
 userController.registerUser
 )
 
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min: 6}).withMessage('Password must be atleast 6 characters')
+],
+    userController.loginUser
+)
+
 
 module.exports = router;
